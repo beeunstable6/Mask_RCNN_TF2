6f247ad00c18bb129902c36bbd7a02955fcd6fc7
Lukas <50877600+beeunstable6@users.noreply.github.com>
2021-02-01 13:09:53 +0200

    Update balloon1.py

--- samples/balloon/balloon1.py
+++ samples/balloon/balloon1.py
@@ -65,7 +65,7 @@ class BalloonConfig(Config):
     IMAGES_PER_GPU = 2
 
     # Number of classes (including background)
-    NUM_CLASSES = 1 + 2  # Background + balloon
+    NUM_CLASSES = 1 + 1  # Background + balloon
 
     # Number of training steps per epoch
     STEPS_PER_EPOCH = 100
@@ -73,6 +73,7 @@ class BalloonConfig(Config):
     # Skip detections with < 90% confidence
     DETECTION_MIN_CONFIDENCE = 0.9
 
+    LEARNING_RATE = 0.0005
 
 ############################################################
 #  Dataset
@@ -86,8 +87,7 @@ class BalloonDataset(utils.Dataset):
         subset: Subset to load: train or val
         """
         # Add classes. We have only one class to add.
-        self.add_class("balloon", 1, "cat")
-        self.add_class("balloon", 2, "dog")
+        self.add_class("balloon", 1, "dog")
 
         # Train or validation dataset?
         assert subset in ["train", "val"]
@@ -121,7 +121,6 @@ class BalloonDataset(utils.Dataset):
             # the outline of each object instance. There are stores in the
             # shape_attributes (see json format above)
             polygons = [r['shape_attributes'] for r in a['regions']]
-            names = [r['region_attributes'] for r in a ['regions']]
 
             # load_mask() needs the image size to convert polygons to masks.
             # Unfortunately, VIA doesn't include it in JSON, so we must read
@@ -130,13 +129,12 @@ class BalloonDataset(utils.Dataset):
             image = skimage.io.imread(image_path)
             height, width = image.shape[:2]
 
-              
             self.add_image(
                 "balloon",
                 image_id=a['filename'],  # use file name as a unique image id
                 path=image_path,
                 width=width, height=height,
-                polygons=polygons, names=names)
+                polygons=polygons)
 
     def load_mask(self, image_id):
         """Generate instance masks for an image.
@@ -153,7 +151,6 @@ class BalloonDataset(utils.Dataset):
         # Convert polygons to a bitmap mask of shape
         # [height, width, instance_count]
         info = self.image_info[image_id]
-        class_names = info['names']
         mask = np.zeros([info["height"], info["width"], len(info["polygons"])],
                         dtype=np.uint8)
         for i, p in enumerate(info["polygons"]):
@@ -161,17 +158,9 @@ class BalloonDataset(utils.Dataset):
             rr, cc = skimage.draw.polygon(p['all_points_y'], p['all_points_x'])
             mask[rr, cc, i] = 1
 
-        class_ids = np.zeros([len(info["polygons"])])
-        for i, p in enumerate(class_names):
-              if p['balloon'] == 'cat':
-                     class_ids[i] = 1
-              elif p['balloon'] == 'dog':
-                     class_ids[i] = 2
-        
-        class_ids = class_ids.astype(int)
         # Return mask, and array of class IDs of each instance. Since we have
         # one class ID only, we return an array of 1s
-        return mask.astype(np.bool), class_ids
+        return mask.astype(np.bool), np.ones([mask.shape[-1]], dtype=np.int32)
 
     def image_reference(self, image_id):
         """Return the path of the image."""
@@ -201,13 +190,8 @@ def train(model):
     print("Training network heads")
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
-                epochs=30,
+                epochs=500,
                 layers='heads')
-
-    model.train(dataset_train, dataset_val,
-                learning_rate=config.LEARNING_RATE,
-                epochs=45,
-                layers='all')
 
 
 def color_splash(image, mask):
@@ -234,7 +218,7 @@ def detect_and_color_splash(model, image_path=None, video_path=None):
 
     # Image or video?
     if image_path:
-        class_names=['BG','cat','dog']
+        class_names=['BG','dog']
         # Run model detection and generate the color splash effect
         print("Running on {}".format(args.image))
         # Read image
@@ -370,9 +354,7 @@ if __name__ == '__main__':
             "mrcnn_class_logits", "mrcnn_bbox_fc",
             "mrcnn_bbox", "mrcnn_mask"])
     else:
-        model.load_weights(weights_path, by_name=True, exclude=[
-            "mrcnn_class_logits", "mrcnn_bbox_fc",
-            "mrcnn_bbox", "mrcnn_mask"])
+        model.load_weights(weights_path, by_name=True)
 
     # Train or evaluate
     if args.command == "train":
